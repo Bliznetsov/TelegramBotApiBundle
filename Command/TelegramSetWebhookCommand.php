cc1744aebc3f0d9ca5a769bf5fcba44fccdae346
Alireza Seddighi <seddighi.alireza78@gmail.com>
2017-10-08 17:07:47 +0330

    Update TelegramSetWebhookCommand.php

--- Command/TelegramSetWebhookCommand.php
+++ Command/TelegramSetWebhookCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace TelegramBotApiBundle\Command;
+namespace EricomGroup\TelegramBotApiBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
