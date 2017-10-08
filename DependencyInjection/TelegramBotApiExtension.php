9e7c6969d4d31f99481fa53f383d2784b0201680
Alireza Seddighi <seddighi.alireza78@gmail.com>
2017-10-08 17:02:47 +0330

    Update TelegramBotApiExtension.php

--- DependencyInjection/TelegramBotApiExtension.php
+++ DependencyInjection/TelegramBotApiExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace TelegramBotApiBundle\DependencyInjection;
+namespace EricomGroup\TelegramBotApiBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
