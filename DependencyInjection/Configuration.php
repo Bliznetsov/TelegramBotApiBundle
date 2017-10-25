46826f5810076b4791488459e2e053b1c2a4f1b9
Alireza Seddighi <seddighi.alireza78@gmail.com>
2017-10-25 15:37:36 +0330

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -7,22 +7,38 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-
     /**
      * {@inheritdoc}
      */
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('telegram_bot_api');
-
-
-        $rootNode
+	    $treeBuilder->root('telegram_bot_api')
                 ->children()
-                    ->scalarNode("token")->isRequired()->end()
-                    ->scalarNode("bot_name")->end()
+			        ->scalarNode( 'username' )
+		            ->end()
+			        ->scalarNode( 'token' )
+				        ->isRequired()
+			        ->end()
+	                ->arrayNode('development')
+	                    ->children()
+	                        ->arrayNode('developers_id')
+	                            ->prototype('scalar')
+		                        ->end()
+                            ->end()
+	                        ->arrayNode('maintenance')
+	                            ->children()
+	                                ->booleanNode('enable')
+	                                    ->defaultFalse()
+                                    ->end()
+	                                ->scalarNode('text')
+	                                    ->defaultValue('The robot is being repaired! Please come back later.')
+                                    ->end()
+	                            ->end()
+	                        ->end()
+	                    ->end()
+	                ->end()
                 ->end();
-
 
         return $treeBuilder;
     }
