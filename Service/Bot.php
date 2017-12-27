52e5a620c90ffa01fda0e15d51f3fd090a7c1177
Alireza Seddighi <seddighi.alireza78@gmail.com>
2017-12-27 14:32:01 +0330

    Update Bot.php

--- Service/Bot.php
+++ Service/Bot.php
@@ -100,7 +100,7 @@ class Bot
 	public function __call( $name, $arguments)
 	{
 		$result =  call_user_func_array(['Longman\TelegramBot\Request', $name], $arguments);
-		if(property_exists($result, 'ok') && !$result->ok)
+		if(property_exists($result, 'ok') && !$result->ok && $result->error_code != 403)
 		{
 			throw new TelegramException($result->description);
 		}
