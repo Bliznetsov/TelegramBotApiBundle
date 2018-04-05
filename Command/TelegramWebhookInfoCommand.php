7cc3f58d6f49269082f8edea7460eadd2fd269da
Alireza Seddighi <seddighi.alireza78@gmail.com>
2018-04-06 01:19:46 +0430

    Update TelegramWebhookInfoCommand.php

--- Command/TelegramWebhookInfoCommand.php
+++ Command/TelegramWebhookInfoCommand.php
@@ -12,9 +12,18 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class TelegramWebhookInfoCommand extends Command
 {
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        parent::__construct();
+        $this->container = $container;
+    }
+
     protected function configure()
     {
         $this
@@ -26,7 +35,7 @@ class TelegramWebhookInfoCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output) {
     	$io = new SymfonyStyle($input, $output);
 
-	    $result = $this->getContainer()->get('telegram_bot_api')->getWebhookInfo();
+	    $result = $this->container->get('telegram_bot_api')->getWebhookInfo();
 	    if($result->isOk())
 	    {
 		    $result = (array) $result->result;
